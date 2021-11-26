cc47e0896196cfad8f988f83a027506c67acd636
MatheusPerillo <c4tr4u1@gmail.com>
2021-11-26 04:03:48 -0300

    Create and stylesheet of the Home/Timer page

--- front/src/Components/Button/index.tsx
+++ front/src/Components/Button/index.tsx
@@ -6,6 +6,7 @@ function index() {
             
         </div>
     )
+
 }
 
 export default index
--- front/src/Pages/Home/index.tsx
+++ front/src/Pages/Home/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./styles.css";
 
 function index() {
-    return (
-        <div>
-            <h1>hello timer</h1>
+  return (
+    <div>
+      <div className="container-global">
+        <div className="container-btn">
+          <button className="btn-sidebar">
+            <p>Side</p>
+          </button>
         </div>
-    )
+        <div className="container-texts">
+          <div className="pomodorc-text">
+            <h1>POMOD'ORC</h1>
+          </div>
+          <div className="timer-text">25:00</div>
+          <div className="ctn-btn-play">
+            <button className ="btn-play">||</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default index
-
+export default index;
